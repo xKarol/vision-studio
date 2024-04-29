1fa69872fa29c420ba9ebef0cd7058ac3d5b1b44
Karol <karolpiskorz8@gmail.com>
2024-04-29 18:02:43 +0200

    add header component

---
+++ src/config/routes.ts
@@ -0,0 +1,13 @@
+export const routes = {
+  NOT_FOUND: "/404",
+  HOME: "/",
+  ABOUT: "/about", //TODO not exist
+  PORTFOLIO: "/portfolio", // not exist
+  FAQ: "/faq", // not exist
+  CONTACT: "/contact", // not exist
+  socials: {
+    INSTAGRAM: "https://instagram.com",
+    FACEBOOK: "https://facebook.com",
+    PINTEREST: "https://pinterest.com",
+  },
+} as const;
