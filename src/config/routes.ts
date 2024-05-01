07a93cd855a8ed868dd04297b4308bbdec3696b6
Karol <karolpiskorz8@gmail.com>
2024-05-01 21:07:21 +0200

    rename portfolio with work

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,7 +2,6 @@ export const routes = {
   NOT_FOUND: "/404",
   HOME: "/",
   ABOUT: "/about", //TODO not exist
-  PORTFOLIO: "/portfolio", // not exist
   FAQ: "/faq", // not exist
   CONTACT: "/contact", // not exist
   socials: {
@@ -10,8 +9,8 @@ export const routes = {
     FACEBOOK: "https://facebook.com",
     PINTEREST: "https://pinterest.com",
   },
-  portfolio: {
-    DEFAULT: "/portfolio",
-    slug: (slug: string) => `/portfolio/${slug}`,
+  work: {
+    DEFAULT: "/work",
+    slug: (slug: string) => `/work/${slug}`,
   },
 } as const;
