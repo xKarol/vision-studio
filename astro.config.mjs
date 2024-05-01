44843df032fd83fde69bd66aa22c7929a36cf692
Karol <karolpiskorz8@gmail.com>
2024-05-01 17:25:42 +0200

    add storyblok cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,34 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        heading: "storyblok/Heading",
+      },
+    }),
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
   ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
