e7e2976e9ddf9d417b9a8e6e5cb37c3bbee8371f
Karol <karolpiskorz8@gmail.com>
2024-05-03 15:49:43 +0200

    remove faq page

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,7 +2,6 @@ export const routes = {
   NOT_FOUND: "/404",
   HOME: "/",
   ABOUT: "/about", //TODO not exist
-  FAQ: "/faq", // not exist
   CONTACT: "/contact", // not exist
   socials: {
     INSTAGRAM: "https://instagram.com",
