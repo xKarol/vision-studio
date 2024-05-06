8d0575cf66daff9e31451e2b16c302c8c033263c
Karol Piskorz <karolpiskorz8@gmail.com>
2024-05-06 14:51:22 +0200

    use getSiteUrl function in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import { getSiteUrl } from "./src/lib/get-site-url";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
@@ -6,12 +7,13 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 
-const env = loadEnv("", process.cwd(), "STORYBLOK");
+const env = loadEnv("", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  site: getSiteUrl(),
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
--- src/lib/get-site-url.ts
+++ src/lib/get-site-url.ts
@@ -1,11 +1,12 @@
 export const getSiteUrl = () => {
-    // custom
-    if (process.env.ORIGIN_URL) {
-      return new URL(process.env.ORIGIN_URL).toString();
-    }
-    // vercel
-    if (process.env.VERCEL && process.env.VERCEL_URL) {
-      return new URL(`https://${process.env.VERCEL_URL}`).toString();
-    }
-    return new URL("http://localhost:4321").toString();
-  };
+  console.log(process.env.ORIGIN_URL, import.meta.env);
+  // custom
+  if (process.env.ORIGIN_URL) {
+    return new URL(process.env.ORIGIN_URL).toString();
+  }
+  // vercel
+  if (process.env.VERCEL && process.env.VERCEL_URL) {
+    return new URL(`https://${process.env.VERCEL_URL}`).toString();
+  }
+  return new URL("http://localhost:4321").toString();
+};
