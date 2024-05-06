e4e2a1c0f742b081e9c591649e85bb088afff9dd
Karol Piskorz <karolpiskorz8@gmail.com>
2024-05-06 14:53:32 +0200

    update env.d.ts file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly ORIGIN_URL: string;
+  readonly STORYBLOK_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
