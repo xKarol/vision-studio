46301ee8b5ce0e457c4edf3bf3a835b2d6e4e3b9
Karol Piskorz <karolpiskorz8@gmail.com>
2024-05-09 19:12:39 +0200

    render WorkSwiper images in lower resolution

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function transformStoryblokImage(
+  url: string,
+  width: number,
+  height: number,
+) {
+  return `${url}/m//fit-in/${width}x${height}/filters:quality(100)`;
 }
